94b9f64ee8f2808815827f8c1afcea00b00b4420
Tushar Bhatt <bhatttushar182@gmail.com>
2025-07-06 18:59:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os.path
+import os
 import datetime
 import pickle
-
 import tkinter as tk
 import cv2
 from PIL import Image, ImageTk
@@ -9,7 +8,6 @@ import face_recognition
 
 import util
 from test import test
-
 
 class App:
     def __init__(self):
@@ -22,8 +20,9 @@ class App:
         self.logout_button_main_window = util.get_button(self.main_window, 'logout', 'red', self.logout)
         self.logout_button_main_window.place(x=750, y=300)
 
-        self.register_new_user_button_main_window = util.get_button(self.main_window, 'register new user', 'gray',
-                                                                    self.register_new_user, fg='black')
+        self.register_new_user_button_main_window = util.get_button(
+            self.main_window, 'register new user', 'gray', self.register_new_user, fg='black'
+        )
         self.register_new_user_button_main_window.place(x=750, y=400)
 
         self.webcam_label = util.get_img_label(self.main_window)
@@ -39,78 +38,66 @@ class App:
 
     def add_webcam(self, label):
         if 'cap' not in self.__dict__:
-            self.cap = cv2.VideoCapture(2)
-
+            self.cap = cv2.VideoCapture(0)  # Change index if needed
         self._label = label
         self.process_webcam()
 
     def process_webcam(self):
         ret, frame = self.cap.read()
-
         self.most_recent_capture_arr = frame
         img_ = cv2.cvtColor(self.most_recent_capture_arr, cv2.COLOR_BGR2RGB)
         self.most_recent_capture_pil = Image.fromarray(img_)
         imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
         self._label.imgtk = imgtk
         self._label.configure(image=imgtk)
-
         self._label.after(20, self.process_webcam)
 
     def login(self):
-
         label = test(
-                image=self.most_recent_capture_arr,
-                model_dir='/home/phillip/Desktop/todays_tutorial/27_face_recognition_spoofing/code/face-attendance-system/Silent-Face-Anti-Spoofing/resources/anti_spoof_models',
-                device_id=0
-                )
-
+            image=self.most_recent_capture_arr,
+            model_dir='./resources/anti_spoof_models',
+            device_id=0
+        )
         if label == 1:
-
             name = util.recognize(self.most_recent_capture_arr, self.db_dir)
-
             if name in ['unknown_person', 'no_persons_found']:
                 util.msg_box('Ups...', 'Unknown user. Please register new user or try again.')
             else:
-                util.msg_box('Welcome back !', 'Welcome, {}.'.format(name))
+                util.msg_box('Welcome back!', f'Welcome, {name}.')
                 with open(self.log_path, 'a') as f:
-                    f.write('{},{},in\n'.format(name, datetime.datetime.now()))
-                    f.close()
-
+                    f.write(f'{name},{datetime.datetime.now()},in\n')
         else:
-            util.msg_box('Hey, you are a spoofer!', 'You are fake !')
+            util.msg_box('Hey, you are a spoofer!', 'You are fake!')
 
     def logout(self):
-
         label = test(
-                image=self.most_recent_capture_arr,
-                model_dir='/home/phillip/Desktop/todays_tutorial/27_face_recognition_spoofing/code/face-attendance-system/Silent-Face-Anti-Spoofing/resources/anti_spoof_models',
-                device_id=0
-                )
-
+            image=self.most_recent_capture_arr,
+            model_dir='./resources/anti_spoof_models',
+            device_id=0
+        )
         if label == 1:
-
             name = util.recognize(self.most_recent_capture_arr, self.db_dir)
-
             if name in ['unknown_person', 'no_persons_found']:
                 util.msg_box('Ups...', 'Unknown user. Please register new user or try again.')
             else:
-                util.msg_box('Hasta la vista !', 'Goodbye, {}.'.format(name))
+                util.msg_box('Hasta la vista!', f'Goodbye, {name}.')
                 with open(self.log_path, 'a') as f:
-                    f.write('{},{},out\n'.format(name, datetime.datetime.now()))
-                    f.close()
-
+                    f.write(f'{name},{datetime.datetime.now()},out\n')
         else:
-            util.msg_box('Hey, you are a spoofer!', 'You are fake !')
-
+            util.msg_box('Hey, you are a spoofer!', 'You are fake!')
 
     def register_new_user(self):
         self.register_new_user_window = tk.Toplevel(self.main_window)
         self.register_new_user_window.geometry("1200x520+370+120")
 
-        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user)
+        self.accept_button_register_new_user_window = util.get_button(
+            self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user
+        )
         self.accept_button_register_new_user_window.place(x=750, y=300)
 
-        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try again', 'red', self.try_again_register_new_user)
+        self.try_again_button_register_new_user_window = util.get_button(
+            self.register_new_user_window, 'Try again', 'red', self.try_again_register_new_user
+        )
         self.try_again_button_register_new_user_window.place(x=750, y=400)
 
         self.capture_label = util.get_img_label(self.register_new_user_window)
@@ -121,7 +108,9 @@ class App:
         self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)
         self.entry_text_register_new_user.place(x=750, y=150)
 
-        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window, 'Please, \ninput username:')
+        self.text_label_register_new_user = util.get_text_label(
+            self.register_new_user_window, 'Please, \ninput username:'
+        )
         self.text_label_register_new_user.place(x=750, y=70)
 
     def try_again_register_new_user(self):
@@ -131,7 +120,6 @@ class App:
         imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
         label.imgtk = imgtk
         label.configure(image=imgtk)
-
         self.register_new_user_capture = self.most_recent_capture_arr.copy()
 
     def start(self):
@@ -139,17 +127,20 @@ class App:
 
     def accept_register_new_user(self):
         name = self.entry_text_register_new_user.get(1.0, "end-1c")
-
-        embeddings = face_recognition.face_encodings(self.register_new_user_capture)[0]
-
-        file = open(os.path.join(self.db_dir, '{}.pickle'.format(name)), 'wb')
-        pickle.dump(embeddings, file)
-
-        util.msg_box('Success!', 'User was registered successfully !')
-
+        encodings = face_recognition.face_encodings(self.register_new_user_capture)
+        if not encodings:
+            util.msg_box('Error', 'No face detected. Please try again.')
+            return
+        embeddings = encodings[0]
+        with open(os.path.join(self.db_dir, f'{name}.pickle'), 'wb') as file:
+            pickle.dump(embeddings, file)
+        util.msg_box('Success!', 'User was registered successfully!')
         self.register_new_user_window.destroy()
-
 
 if __name__ == "__main__":
     app = App()
     app.start()
+
+
+
+     
